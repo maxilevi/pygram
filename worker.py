41ed9c73ade4384f1471505c24c18b7ec6f13e2d
Zaphyk <zaphyk@live.com>
2018-09-09 19:31:06 -0300

    get an scheduled image and upload it

--- worker.py
+++ worker.py
@@ -1,18 +1,23 @@
 from InstagramAPI import InstagramAPI
+import schedule
 
 
-class Grammer:
+class Instagrammer:
 
     def __init__(self, config: dict):
-        self.InstagramAPI = InstagramAPI(config['username'], config['password'])
-        self.candidates = self.load_candidates()
-
-    def upload(self, path: str):
-        self.InstagramAPI.uploadPhoto(path, caption='')
+        self.instagram_api = InstagramAPI(config['username'], config['password'])
+        self.schedule_items = schedule.load()
 
     def start(self):
-        self.InstagramAPI.login()
+        self.instagram_api.login()
+        upload_item_key = list(self.schedule_items.keys())[0]
+        self.upload(self.schedule_items[upload_item_key])
+        del self.schedule_items[upload_item_key]
+        schedule.save(self.schedule_items)
 
-    def load_candidates(self) -> list:
-        candidates = []
-        return candidates
+    def upload(self, upload_options: dict):
+        self.instagram_api.uploadPhoto(upload_options['image'], caption=Instagrammer.create_caption(upload_options))
+
+    @staticmethod
+    def create_caption(upload_options: dict):
+        return f"{upload_options['caption']}\n.\n.#{' #'.join(upload_options['tags'])}"
