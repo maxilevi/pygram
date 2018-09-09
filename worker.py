9aaa839bfe4ddfde0b4f0ef52cdfbd8bb295b281
Zaphyk <zaphyk@live.com>
2018-09-09 20:14:12 -0300

    use tags from config

--- worker.py
+++ worker.py
@@ -1,23 +1,28 @@
 from InstagramAPI import InstagramAPI
-import schedule
+import scheduler
 
 
 class Instagrammer:
 
     def __init__(self, config: dict):
         self.instagram_api = InstagramAPI(config['username'], config['password'])
-        self.schedule_items = schedule.load()
+        self.schedule_items = scheduler.load()
+        self.tags = config['tags']
 
     def start(self):
+        if len(self.schedule_items) == 0:
+            raise ValueError("There are no scheduled images.")
         self.instagram_api.login()
         upload_item_key = list(self.schedule_items.keys())[0]
         self.upload(self.schedule_items[upload_item_key])
         del self.schedule_items[upload_item_key]
-        schedule.save(self.schedule_items)
+        scheduler.save(self.schedule_items)
 
     def upload(self, upload_options: dict):
-        self.instagram_api.uploadPhoto(upload_options['image'], caption=Instagrammer.create_caption(upload_options))
+        caption = Instagrammer.create_caption(upload_options, self.tags)
+        print(f"Uploading image with caption:\n{caption}")
+        self.instagram_api.uploadPhoto(upload_options['image'], caption=caption)
 
     @staticmethod
-    def create_caption(upload_options: dict):
-        return f"{upload_options['caption']}\n.\n.#{' #'.join(upload_options['tags'])}"
+    def create_caption(upload_options: dict, tags: list):
+        return f"{upload_options['caption']}\n.\n.\n{(' #'.join(upload_options['tags'] + tags)).strip()}"
